e721a922d79ea5882fd1b39baf377453ba219341
LopezTw <caiohg_94@hotmail.com>
2023-08-14 12:59:09 -0300

    Criaçao do Modelo de Dominio e ORM

---
+++ src/main/java/com/lopeztw/ltcommerce/LtcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.lopeztw.ltcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lopeztw/ltcommerce/LtcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lopeztw.ltcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
